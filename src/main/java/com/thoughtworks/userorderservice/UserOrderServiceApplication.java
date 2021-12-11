4a1a9b6a1658be32ef035f5073ce80f930aeb130
yaowang <Yao.Wang1@cat.com>
2021-12-11 16:52:21 +0800

    feature: code base

---
+++ src/main/java/com/thoughtworks/userorderservice/UserOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.userorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserOrderServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UserOrderServiceApplication.class, args);
+  }
+
+}
